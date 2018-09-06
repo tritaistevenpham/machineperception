ad9e596191b1bd62ab1960f8b611b2bdacf9811a
Steven Pham <stevenphamstuh@gmail.com>
2018-09-06 22:58:45 +0800

    Master main - options start

--- assignment.py
+++ assignment.py
@@ -8,11 +8,20 @@ import cv2
 import numpy as np
 
 ## Main
+fileLoc = ''
+option = 0
+## Choose: 1 - Read a single file from SetA
+##         2 - Read all images from a directory
 
-## Choose: 1 - Read a single file 
-##         2 - Read from a directory
-fileLoc = './20180826a/SetA/label-7-radioactive-ii.png'
-dirLoc = './20180826a/SetA/'
+while option != 1 and option != 2:
+    try: option = int(raw_input( 'Enter either 1 or 2\n 1 - Read a single file from SetA\n 2 - Read all images from directory\n'))
+    except ValueError:
+        print('Please only use integers')
+
+if option == 1:
+    fileLoc = './20180826a/SetA/label-7-radioactive-ii.png'
+elif option == 2:
+    fileLoc = './20180826a/SetA/'
 
 ## Option 1
 img = cv2.imread( fileLoc, cv2.IMREAD_COLOR)
@@ -20,43 +29,11 @@ img = cv2.imread( fileLoc, cv2.IMREAD_COLOR)
 ## Option 2
 ## img = cv2.imread( dirLoc, cv2.IMREAD_COLOR)
 
-## Color conversion
+## Color conversion - grayscale for image processing
 gray = cv2.cvtColor( img, cv2.COLOR_BGR2GRAY)
-
-## Detections
-laplac = cv2.Laplacian( gray, cv2.CV_64F)
-edges = cv2.Canny( gray, 100, 200)
-
-##test commit
-
-blobDetect = cv2.SimpleBlobDetector_create()
-keypoints = blobDetect.detect(gray)
-im_with_keypoints = cv2.drawKeypoints(gray, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-## Image Filter
-##img_filt = cv2.medianBlur(gray, 5)
-##img_th = cv2.adaptiveThreshold( img_filt, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-##contours, hierarchy, _ = cv2.findContours(img_th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-ret, thresh = cv2.threshold(gray, 127,255,0)
-contours, hierarchy, _ = cv2.findContours(thresh,1,2)
-
-## Draw Countour
-cnt = contours[0]
-x,y,w,h = cv2.boundingRect(cnt)
-img = cv2.rectangle( gray, (x,y), (x+w,y+h), (0,255,0), 2)
-
-rect = cv2.minAreaRect(cnt)
-box = cv2.boxPoints(rect)
-box = np.int0(box)
-img = cv2.drawContours(img, [box], 0, (0, 255, 0), 2)
-##img = cv2.drawContours(img, cnt, 0, (0,255,0), 3)
 
 ## Show Image
 cv2.imshow( 'Original Image', img)
-##cv2.imshow( 'Laplacian', laplac)
-cv2.imshow( 'Canny', edges)
-cv2.imshow( 'Keypoints', im_with_keypoints)
 
 ## Destroy Windows
 cv2.waitKey(0)
