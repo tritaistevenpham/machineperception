943aeaa8407ef8e556353cf326a614a874b77207
Steven Pham <stevenphamstuh@gmail.com>
2018-10-03 01:49:45 +0800

    SET A SET B contour

--- assignment.py
+++ assignment.py
@@ -23,25 +23,31 @@ outputDir = 'Output/'
 def preprocessImage(img):
     
     img_eq = cv2.cvtColor( img, cv2.COLOR_BGR2GRAY)
-    clahe = cv2.createCLAHE( clipLimit=4.0, tileGridSize=( 8, 8))
-    cl1 = clahe.apply( img_eq)
+    #Create
+    clahe = cv2.createCLAHE( clipLimit=3.5, tileGridSize=( 8, 8))
+    cl = clahe.apply(img_eq)
     
     ## Color conversion - grayscale for image processing
     #gray_img = cv2.cvtColor( img, cv2.COLOR_BGR2GRAY)
     
     ## Goal: Contour the diamond-shaped hazmat sign to be the main processing space.    
     ## Apply Gaussian Blur and add this weight to the grayscale image
-    gray_img = cv2.GaussianBlur( cl1, (3,3), 1)
-    #gray_img = cv2.addWeighted( gblur, 1.5, gray_img, -0.5, 0, gray_img)
+    #cl1 = cv2.medianBlur( cl1, 5)
+    blur = cv2.GaussianBlur( cl, (5,5), 0)
+    
+    #kernel = np.array( [ [0,-1,0], [-1,5,-1], [0,-1,0]])
+    #dest = cv2.filter2D( gray_img, -1, kernel)
+    
+    sharpen = cv2.addWeighted( img_eq, 0.8, blur, 0.2, 0)
     
     
     ##      Apply Bilateral Filter on grayscale image & run Canny edge detector
-    bfilt = cv2.bilateralFilter( gray_img, 5, 75, 75)
-    thresh = cv2.Canny( bfilt, 255/3, 255)
-    thresh = cv2.dilate( thresh, np.ones((5,5), np.uint8), iterations=1)
+    bfilt = cv2.bilateralFilter( sharpen, 5, 75, 75)
+    thresh = cv2.Canny( bfilt, 255/3, 200)
+    dilated = cv2.dilate( thresh, np.ones((3, 3), np.uint8), iterations=1)
     
     ##      Find the contours within the computed edge map; sort by size in desc order
-    contours = cv2.findContours( thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = cv2.findContours( dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnt = contours[1]
     cnt = sorted(cnt, key=cv2.contourArea, reverse=True)
     
@@ -49,28 +55,35 @@ def preprocessImage(img):
     cont = max( cnt, key=cv2.contourArea)
     
     # Initialise signDetect
-    signDetect = SignDetector()
+    #signDetect = SignDetector()
     
     for c in cnt:
-        sign = signDetect.findSign( c)
-        if( sign == True):
-            # Draw the sign contour
-            cv2.drawContours( img, [c], -1, (0, 255, 0), 2)
+        #sign = signDetect.findSign( c)
+        perimeter = cv2.arcLength( c, True)
+        aprx = cv2.approxPolyDP( c, 0.02 * perimeter, True)
         
-        #cv2.drawContours( img, [aprx], 0, (0, 0, 255), 4)
+        # If the contour detected has 4 vertices; likely to be our sign:
+        if ( len( aprx) == 4) and cv2.contourArea( aprx) > float(4000.0):
+            # Compute the bounding box
+            sign = aprx
+            
+        # Draw the sign contour
+        cv2.drawContours( img, [sign], -1, (0, 255, 0), 3)
+        
+        #cv2.drawContours( img, c, 0, (0, 0, 255), 4)
         
     ## Draw mask
     mask = np.zeros( img.shape, np.uint8)
-    cv2.drawContours( mask, cnt, 0, (255,255,255), 3)
+    #cv2.drawContours( mask, cnt, 0, (255,255,255), 3)
     
     ## Draw contours on img
-    #cv2.drawContours( img, aprx, -1, (0,0,255), 5)
+    #cv2.drawContours( mask, [sign], -1, (0,0,255), 2)
     
     ##      Draw the bounding box for testing 
     #x, y, w, h = cv2.boundingRect( cont)
     #cv2.rectangle( img, (x,y), (x+w, y+h), (255, 0, 0), 2)
 
-    return mask
+    return img
 
 ### END-MASK DETECTION
 
@@ -108,12 +121,12 @@ elif option == 2:
     contourC = 'results/contourC/img-'
     contourD = 'results/contourD/img-'
     
-    maxWidth = 500
-    maxHeight = 500
+    maxWidth = 900
+    maxHeight = 900
     
     
     #For now, change this for file location
-    images = glob.glob(fileLocA)
+    images = glob.glob(fileLocC)
     
     data = []
     for files in images:
@@ -129,8 +142,9 @@ elif option == 2:
             
             #dim =  (1000,  int( img_orig.shape[0] * r))
             img = cv2.resize( img_orig, None, fx=r, fy=r, interpolation = cv2.INTER_AREA)
-            
-        data.append( img)
+            data.append(img)
+        else:
+            data.append(img_orig)
     
     ## Option 2
     idx = 0
@@ -138,9 +152,9 @@ elif option == 2:
         try:
             result = preprocessImage(im)
             #Change contour output location & image type here
-            print(outputImages + contourA + str(idx) + png)
+            print(outputImages + contourC + str(idx) + jpg)
             
-            cv2.imwrite( (outputImages + contourA + str(idx) + png), result)
+            cv2.imwrite( (outputImages + contourC + str(idx) + jpg), result)
             idx += 1
         except Exception as e:
             print(e)
